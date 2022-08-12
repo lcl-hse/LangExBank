c5391120c6ac7ff7e72a0a580c439c88d39e626e
nicklogin <niklogin10v@yandex.ru>
2022-08-12 16:56:34 +0300

    started necessary adjustments for linghub

---
+++ LangExBank/entrypoint.sh
@@ -0,0 +1,14 @@
+echo "Waiting for postgres..."
+
+while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python manage.py migrate
+
+python manage.py collectstatic --no-input --clear
+
+# cкачать данные с хостинга, используя пароль из environment_variables
+# загрузить их с помощью loaddata
